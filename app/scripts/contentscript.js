b939121aa0dc5ad29efbba5edaa434f04b6980b3
Jerome Botineau <j.botineau@vtech.fr>
2013-11-17 15:22:05 +0100

    Clean code

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -3,7 +3,6 @@
 chrome.storage.sync.get('bookmark', function(value) {
   var icon = 'icon-128-2.png';
   var folder = 'images/';
-//  var n = value.indexOf("welcome");
 
   chrome.runtime.sendMessage({ "newIconPath" : folder+icon });
 });
