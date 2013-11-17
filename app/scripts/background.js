17aeb432807696c77924cf9e4a0fc8aceb311f16
Jerome Botineau <j.botineau@vtech.fr>
2013-11-17 13:47:08 +0100

    change icon

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -5,5 +5,18 @@ chrome.runtime.onInstalled.addListener(function (details) {
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId) {
-    chrome.pageAction.show(tabId);
+	    chrome.pageAction.show(tabId);    
 });
+
+
+chrome.tabs.executeScript({file: "content.js"});
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+	    // read `newIconPath` from request and read `tab.id` from sender
+	    console.log(chrome.pageAction);
+	    chrome.pageAction.setIcon({
+	        path: request.newIconPath,
+	        tabId: sender.tab.id
+	    });
+});
+
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,6 @@
 'use strict';
+chrome.storage.sync.get('bookmark', function(value) {
+  var icon = 'icon-38-2.png';
+  var folder = 'images/';
+  chrome.runtime.sendMessage({ "newIconPath" : folder+icon });
+});
